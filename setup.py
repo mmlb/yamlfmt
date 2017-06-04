1fb0aa578ea753f4c271424010bf993bc09c638b
Anthon van der Neut <anthon@mnt.org>
2017-06-04 16:55:50 +0200

    fix ruamel.yaml on 0.14.X for now
    Thank you for using ruamel.yaml. 
    
    There will be API changes in the 0.15+ versions, that might lead to warnings that 
    your users could see. 
    Therefore please release a version of your package with this change, so that it will not 
    automatically take the latest ruamel.yaml release.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         version='0.1.4',
         scripts=['yamlfmt'],
         license='[MPLv2.0](https://mozilla.org/MPL/2.0/)',
-        install_requires=['ruamel.yaml'],
+        install_requires=['ruamel.yaml<0.15'],
         classifiers=[
             'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
             'Programming Language :: Python :: 3',
