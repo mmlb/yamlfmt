0956fcb7f87d11f86e1d583236e29abe0f19b9b9
Manuel Mendez <mmendez534@gmail.com>
2018-05-06 16:27:55 -0400

    setup.py: sort kwargs

--- setup.py
+++ setup.py
@@ -6,21 +6,23 @@ try:
 except ImportError:
     long_description = open('README.md').read()
 
+classifiers = [
+    'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+    'Programming Language :: Python :: 3',
+    'Topic :: Utilities',
+]
+
 setup(
-    name='yamlfmt',
-    description='An opinionated yaml formatter based on ruamel.yaml',
-    long_description=long_description,
-    url='https://github.com/mmlb/yamlfmt',
     author='Manuel Mendez',
     author_email='mmendez534@gmail.com',
-    version='0.1.5',
-    scripts=['yamlfmt'],
-    license='[MPLv2.0](https://mozilla.org/MPL/2.0/)',
+    classifiers=classifiers,
+    description='An opinionated yaml formatter based on ruamel.yaml',
     install_requires=['ruamel.yaml<0.15'],
-    classifiers=[
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-        'Programming Language :: Python :: 3',
-        'Topic :: Utilities',
-    ],
     keywords='yaml format',
+    license='[MPLv2.0](https://mozilla.org/MPL/2.0/)',
+    long_description=long_description,
+    name='yamlfmt',
+    scripts=['yamlfmt'],
+    url='https://github.com/mmlb/yamlfmt',
+    version='0.1.5',
 )
