f273b6c9c00d79916af3b82d7d7edf6a294fd678
Manuel Mendez <mmendez534@gmail.com>
2019-03-22 23:49:26 -0400

    bump version and open to py2

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-import sys
 from setuptools import setup
-
-if sys.version_info[0] < 3:
-    sys.stderr.write("Python < 3 is unsupported")
 
 with open("README") as f:
     long_description = f.read()
 
 classifiers = [
     "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 2",
     "Topic :: Utilities",
 ]
 
@@ -19,13 +16,12 @@ setup(
     classifiers=classifiers,
     description="An opinionated yaml formatter based on ruamel.yaml",
     install_requires=["ruamel.yaml<0.15"],
-    keywords="yaml format",
+    keywords="yaml formatter",
     license="MPL 2.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
     name="yamlfmt",
-    python_requires=">=3.0",
     scripts=["yamlfmt"],
     url="https://github.com/mmlb/yamlfmt",
-    version="0.1.6",
+    version="0.1.7",
 )
