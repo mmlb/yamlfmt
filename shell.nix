106c80dfa82bdf7a669097ab24062273d89b3f88
Manuel Mendez <mmendez534@gmail.com>
2019-03-23 00:05:46 -0400

    nix: avoid ZIP errors

--- shell.nix
+++ shell.nix
@@ -1,10 +1,8 @@
-# NOTE: for bdist_wheel zip file errors see:
-# https://github.com/NixOS/nixpkgs-channels/blob/nixos-unstable/doc/languages-frameworks/python.section.md#python-setuppy-bdist_wheel-cannot-create-whl
-
 with import <nixpkgs> { };
 stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
+  SOURCE_DATE_EPOCH = "315532800";
   buildInputs = [
     python3
     python3Packages.black
