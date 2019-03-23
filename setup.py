a79075fb4c0b8b5499e76675c0ec149a9059b2d3
Manuel Mendez <mmendez534@gmail.com>
2019-03-22 23:25:15 -0400

    blackify

--- setup.py
+++ setup.py
@@ -2,30 +2,30 @@ import sys
 from setuptools import setup
 
 if sys.version_info[0] < 3:
-    sys.stderr.write('Python < 3 is unsupported')
+    sys.stderr.write("Python < 3 is unsupported")
 
-with open('README') as f:
+with open("README") as f:
     long_description = f.read()
 
 classifiers = [
-    'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-    'Programming Language :: Python :: 3 :: Only',
-    'Topic :: Utilities',
+    "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+    "Programming Language :: Python :: 3 :: Only",
+    "Topic :: Utilities",
 ]
 
 setup(
-    author='Manuel Mendez',
-    author_email='mmendez534@gmail.com',
+    author="Manuel Mendez",
+    author_email="mmendez534@gmail.com",
     classifiers=classifiers,
-    description='An opinionated yaml formatter based on ruamel.yaml',
-    install_requires=['ruamel.yaml<0.15'],
-    keywords='yaml format',
-    license='MPL 2.0',
+    description="An opinionated yaml formatter based on ruamel.yaml",
+    install_requires=["ruamel.yaml<0.15"],
+    keywords="yaml format",
+    license="MPL 2.0",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    name='yamlfmt',
-    python_requires='>=3.0',
-    scripts=['yamlfmt'],
-    url='https://github.com/mmlb/yamlfmt',
-    version='0.1.6',
+    long_description_content_type="text/markdown",
+    name="yamlfmt",
+    python_requires=">=3.0",
+    scripts=["yamlfmt"],
+    url="https://github.com/mmlb/yamlfmt",
+    version="0.1.6",
 )
